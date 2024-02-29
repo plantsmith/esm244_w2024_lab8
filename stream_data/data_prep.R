979868dce8052a3b9ded62c31beb1745e98e87dd
Casey O'Hara <cohara@ucsb.edu>
2024-02-29 10:51:11 -0800

    updating lab and setting up for post-lab

--- stream_data/data_prep.R
+++ stream_data/data_prep.R
@@ -1,14 +1,37 @@
 library(tidyverse)
 library(here)
+library(ggfortify)
 
-stream_data_subset <- read_csv(here('stream_data', 'sbc_lter_registered_stream_chemistry.csv'),
-                            na = '-999') %>%
+set.seed(12345)
+stream_data_raw <- read_csv(here('stream_data', 'sbc_lter_registered_stream_chemistry.csv'),
+                            na = '-999') 
+
+stream_data_subset <- stream_data_raw %>%
   select(-contains(c('tss'))) %>%
-  drop_na() %>%
-  filter(nh4_uM > 0 & no3_uM > 0 & po4_uM > 0 & tdn_uM > 0 & tdp_uM > 0 & spec_cond_uSpercm > 0) %>%
-  filter(tpp_uM > 0)
+  pivot_longer(cols = c(-site_code, -timestamp_local)) %>%
+  filter(value > 0) %>%
+  group_by(site_code, timestamp_local, name) %>%
+  summarize(value = mean(value, na.rm = TRUE), .groups = 'drop') %>%
+  pivot_wider() %>%
+  drop_na(tdp_uM, nh4_uM) %>%
+  sample_frac(0.2)
+
+
+summary(stream_data_subset)
 
 write_csv(stream_data_subset, here('data/stream_data_subset.csv'))
+
+stream_data_nas <- stream_data_subset %>%
+  pivot_longer(where(is.numeric)) %>%
+  mutate(year = year(timestamp_local)) %>%
+  mutate(value = is.na(value)) %>%
+  group_by(site_code, year) %>%
+  summarize(n_na = sum(value),
+            p_na = sum(value) / n())
+ggplot(stream_data_nas, aes(x = year, y = p_na)) +
+  geom_line() +
+  facet_wrap(~site_code)
+
 
 ggplot(stream_data_subset %>% pivot_longer(cols = where(is.numeric))) +
   geom_histogram(aes(x = value)) +
@@ -21,3 +44,4 @@ stream_pca <- stream_data_subset %>%
   prcomp(scale = TRUE)
 
 autoplot(stream_pca, loadings = TRUE, loadings.label = TRUE)
+screeplot(stream_pca, type = 'lines')
