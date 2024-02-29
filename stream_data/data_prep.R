9eb9b629db410e7a670f410faec1b6ba579d2a58
Casey O'Hara <cohara@ucsb.edu>
2024-02-28 23:04:09 -0800

    saving out stream data subset

--- stream_data/data_prep.R
+++ stream_data/data_prep.R
@@ -1,18 +1,20 @@
 library(tidyverse)
 library(here)
 
-stream_data_raw <- read_csv(here('stream_data', 'sbc_lter_registered_stream_chemistry.csv'),
+stream_data_subset <- read_csv(here('stream_data', 'sbc_lter_registered_stream_chemistry.csv'),
                             na = '-999') %>%
   select(-contains(c('tss'))) %>%
   drop_na() %>%
   filter(nh4_uM > 0 & no3_uM > 0 & po4_uM > 0 & tdn_uM > 0 & tdp_uM > 0 & spec_cond_uSpercm > 0) %>%
   filter(tpp_uM > 0)
 
-ggplot(stream_data_raw %>% pivot_longer(cols = where(is.numeric))) +
+write_csv(stream_data_subset, here('data/stream_data_subset.csv'))
+
+ggplot(stream_data_subset %>% pivot_longer(cols = where(is.numeric))) +
   geom_histogram(aes(x = value)) +
   facet_wrap(~name, scales = 'free')
 
-stream_pca <- stream_data_raw %>%
+stream_pca <- stream_data_subset %>%
   select(-site_code, -timestamp_local) %>%
   mutate(across(where(is.numeric), ~log(.x))) %>%
   drop_na() %>%
